4c068bb73856af7f749ca2eb93289e8c83688216
Lipset <lipsetpatel@gmail.com>
2026-09-21 00:32:49 +0800

    Update zh-cn.js

--- zh-cn.js
+++ zh-cn.js
@@ -1,7 +1,7 @@
 /**
  name         AO3 Translator - zh-cn
  namespace    https://github.com/V-Lipset/ao3-chinese
- version      1.10.0-2026-09-20
+ version      1.10.1-2026-09-21
  description  AO3 Translator 的词库文件
  author       V-Lipset
  license      GPL-3.0
